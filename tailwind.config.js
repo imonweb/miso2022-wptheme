7eec42c208df381861bdc1610607aec4f25ca9e8
Imon <contact@imonweb.co.uk>
2022-05-15 13:50:44 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [
+    './assets/src/scripts/**/*.js',
+    './views/**/*.php',
+    './page-templates/**/*.php',
+    './inc/**/*.php',
+    './404.php',
+    './archive.php',
+    './comments.php',
+    './footer.php',
+    './functions.php',
+    './header.php',
+    './index.php',
+    './page.php',
+    './search.php',
+    './searchform.php',
+    './sidebar.php',
+    './single.php'
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
